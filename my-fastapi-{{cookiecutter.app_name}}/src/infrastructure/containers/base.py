23d7a0835e9203d4a93c4bdc01066aa125ab744f
Max <maxim@akimkin>
2025-01-17 19:31:48 +0100

    🟩 feat: connection manager added to the di container

--- my-fastapi-{{cookiecutter.app_name}}/src/infrastructure/containers/base.py
+++ my-fastapi-{{cookiecutter.app_name}}/src/infrastructure/containers/base.py
@@ -2,11 +2,14 @@
 # region CONTAINER BASE
 # ----------------------------------------------------------------------------------
 from settings.config import Config
+from typing import AsyncIterable
 
 from dishka import Scope, Provider, provide
 
 from ..authenticator.base import AuthenticatorBase
 from ..authenticator.argon import ArgonAuthenticator
+from ..connection_manager.common.base import ConnectionManagerBase
+from ..connection_manager.fastapi.concurrent import FastAPIConnectionManagerConcurrent
 
 
 class ContainerBase(Provider):
@@ -14,6 +17,15 @@ class ContainerBase(Provider):
     def get_config(self) -> Config:
         return Config()
 
+    @provide(scope=Scope.APP)
+    async def get_connection_manager(self) -> AsyncIterable[ConnectionManagerBase]:
+        connection_manager = FastAPIConnectionManagerConcurrent()
+
+        yield connection_manager
+
+        await connection_manager.close()
+
+    @provide(scope=Scope.APP)
     def get_authenticator(self, config: Config) -> AuthenticatorBase:
         return ArgonAuthenticator(
             access_token_secret_key=config.AUTH_ACCESS_TOKEN_SECRET_KEY,
