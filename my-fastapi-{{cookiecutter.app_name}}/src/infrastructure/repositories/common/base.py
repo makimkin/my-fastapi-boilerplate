b6bf35d5a8f8b85e261fc2add664f57d0336eb16
Max <maxim@akimkin>
2025-01-19 14:18:55 +0100

    🟩 feat: base repository inherits from infrastructure base class now

--- my-fastapi-{{cookiecutter.app_name}}/src/infrastructure/repositories/common/base.py
+++ my-fastapi-{{cookiecutter.app_name}}/src/infrastructure/repositories/common/base.py
@@ -1,11 +1,14 @@
 # endregion-------------------------------------------------------------------------
 # region BASE REPOSITORY
 # ----------------------------------------------------------------------------------
+from abc import ABC
 from dataclasses import dataclass
+
+from infrastructure.common.base import InfrastructureBase
 
 
 @dataclass
-class RepositoryBase: ...
+class RepositoryBase(InfrastructureBase, ABC): ...
 
 
 # endregion-------------------------------------------------------------------------
