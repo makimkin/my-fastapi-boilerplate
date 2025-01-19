4fe0a73e9feafb24ce9705e408859a27b9d94a0a
Max <maxim@akimkin>
2025-01-19 14:19:49 +0100

    🟩 feat: removed entity with id

--- my-fastapi-{{cookiecutter.app_name}}/src/domain/common/entity.py
+++ my-fastapi-{{cookiecutter.app_name}}/src/domain/common/entity.py
@@ -4,11 +4,7 @@
 from dataclasses import dataclass, field
 from abc import ABC, abstractmethod
 
-from .value_object import EntityId
 from .event import EventBase
-
-
-KEY_ENTITY_ID = "id"
 
 
 @dataclass(eq=False, kw_only=True)
@@ -31,24 +27,6 @@ class EntityBase(ABC):
     @classmethod
     @abstractmethod
     def from_dict(cls, document: dict) -> "EntityBase": ...
-
-
-@dataclass(eq=False, kw_only=True)
-class EntityBaseWithId(EntityBase, ABC):
-    id: EntityId = field(default_factory=lambda: EntityId.create())
-
-    def __hash__(self) -> int:
-        return hash(self.id.as_raw())
-
-    def __eq__(self, o: object) -> bool:
-        return isinstance(o, self.__class__) and self.id == o.id
-
-    def __post_init__(self):
-        if not self.id:
-            raise ValueError("Entity id must be greater than zero.")
-
-    def to_dict(self) -> dict:
-        return {KEY_ENTITY_ID: self.id.as_raw(), **super().to_dict()}
 
 
 # endregion-------------------------------------------------------------------------
